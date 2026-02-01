d5a11c3699fb4829cd73acde060fe263669d4b66
Ahmad Fachri <152162446+Ahmadfachri12@users.noreply.github.com>
2026-02-01 18:30:26 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,21 +4,18 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
+
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0.0"
+      version = ">= 3.0.0"
     }
   }
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
-  insecure    = true 
+  config_path = "/var/jenkins_home/.kube/config"
 }
 
 provider "helm" {
-  kubernetes {
-    config_path = "~/.kube/config"
-    insecure    = true 
-  }
+  config_path = "/var/jenkins_home/.kube/config"
 }
