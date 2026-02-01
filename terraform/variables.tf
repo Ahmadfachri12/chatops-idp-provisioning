403a2242867887aa84bc931050a5b729728e525b
Ahmad Fachri <152162446+Ahmadfachri12@users.noreply.github.com>
2026-02-01 17:09:31 +0700

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,4 @@
-#######################################
-# Environment Name
-#######################################
 variable "env_name" {
-  description = "Nama environment (dev, staging, prod)"
+  description = "Nama environment / namespace"
   type        = string
-
-  validation {
-    condition     = contains(["dev", "staging", "prod"], var.env_name)
-    error_message = "env_name harus salah satu dari: dev, staging, prod."
-  }
 }
