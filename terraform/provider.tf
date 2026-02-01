518211da82ac1a126b5c106efed562873ed0d553
Ahmad Fachri <152162446+Ahmadfachri12@users.noreply.github.com>
2026-02-01 19:04:22 +0700

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,21 +4,20 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = ">= 2.0.0"
     }
+
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0.0"
+      version = ">= 3.0.0"
     }
   }
 }
 
-# Provider Kubernetes
-# Cukup arahkan ke config path, setting insecure sudah ada di dalam file-nya
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  config_path = "/var/jenkins_home/.kube/config"
 }
 
-# Provider Helm
-# Kosongkan saja bloknya.
-# Provider ini akan otomatis mencari ~/.kube/config dan mengikuti setting yang ada di sana.
 provider "helm" {
+  kubernetes = {
+    config_path = "/var/jenkins_home/.kube/config"
+  }
 }
