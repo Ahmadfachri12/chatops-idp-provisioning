f8007d6d18cb292e674132e539e6022568ae9a8c
Ahmad Fachri <152162446+Ahmadfachri12@users.noreply.github.com>
2026-02-01 17:45:39 +0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,23 @@
-#######################################
-# Kubernetes Namespace (Environment)
-#######################################
-resource "kubernetes_namespace" "env_ns" {
+resource "kubernetes_namespace_v1" "env_ns" {
   metadata {
-    name = "env-${var.env_name}"
-
-    labels = {
-      environment = var.env_name
-      managed-by  = "terraform"
-      project     = "chatops-local"
-    }
+    name = var.env_name
   }
 }
 
-#######################################
-# Helm Release - Nginx
-#######################################
 resource "helm_release" "nginx" {
   name       = "${var.env_name}-nginx"
   repository = "oci://registry-1.docker.io/bitnamicharts"
   chart      = "nginx"
 
-  namespace  = kubernetes_namespace.env_ns.metadata[0].name
+  namespace  = kubernetes_namespace_v1.env_ns.metadata[0].name
   wait       = false
-  timeout    = 300
-  atomic     = false
+
+  depends_on = [
+    kubernetes_namespace_v1.env_ns
+  ]
 
   set {
     name  = "service.type"
     value = "ClusterIP"
   }
-
-  depends_on = [
-    kubernetes_namespace.env_ns
-  ]
 }
